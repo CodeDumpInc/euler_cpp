6aa973d20158cd7177ee743f4ffa0c86cc2848a5
Philipp B <weeska@muhpad.muh>
2013-09-18 00:34:56 +0200

    problem 120

---
+++ Euler_0120.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+typedef long long ResultType;
+
+
+int main(int argc, char* argv[]){
+
+	ResultType sum = 0;	
+	for( ResultType a=3; a <= 1000; ++a){
+		ResultType r_max = 0;
+
+		ResultType a_ = a-1;
+		ResultType b_ = a+1;
+		ResultType r;
+
+		for( ResultType n=1 ; n<a*a; ++n ){
+			r = (a_ + b_) % (a*a);
+			
+			if( r > r_max ) 
+				r_max = r;
+			
+			a_ = (a_ * (a-1)) % (a*a);
+			b_ = (b_ * (a+1)) % (a*a);
+		}
+
+		sum += r_max;
+	}
+	std::cout << sum << std::endl;	
+
+	return 0;
+}
